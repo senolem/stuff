bd92bd0dc1e0e37ee80567833f72c685c5dba5eb
senolem <37041587+senolem@users.noreply.github.com>
2021-12-12 12:59:50 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@ DBNAME = "db"
 echo "Started backup on `date +%d/%m/%Y` at `date +"%T"`"
 echo "Removed backup files : $(rm -rvf ~/backups/*.tgz ~/backups/*.sql > ~/logs/backup.log && wc -l < ~/logs/backup.log)"
 echo "Saved files (logs available at /logs/backup.log) : $(tar -zvcf ~/backups/backup_`date +%d_%m_%Y`.tgz -P ~/public_html > ~/logs/backup.log && wc -l < ~/logs/backup.log)"
-echo "Saved $DBNAME database" && /usr/bin/mysqldump --routines -u user -p'password' dbname > ~/backups/backup_`date +%d_%m_%Y`.sql
+echo "Saved $DBNAME database" && /usr/bin/mysqldump --routines -u user -p'password' $DBNAME > ~/backups/backup_`date +%d_%m_%Y`.sql
 echo "Successfully created backup at `date +"%T"`"
